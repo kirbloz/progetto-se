6a1f9d887873f854c4598add582e079a3fe31830
kirbloz <one.baisini@gmail.com>
2024-10-01 22:42:24 +0200

    implementazione della lettura e scrittura tramite file dei fattori di conversione

--- src/it/unibs/projectIngesoft/attivita/FattoreDiConversione.java
+++ src/it/unibs/projectIngesoft/attivita/FattoreDiConversione.java
@@ -2,15 +2,19 @@ package attivita;
 
 public class FattoreDiConversione {
     private String nome_c1, nome_c2;
-    private float fattore;
+    private double fattore;
 
-    public FattoreDiConversione(String nome_c1, String nome_c2, float fattore){
+    /**
+     *
+     * @param nome_c1, nome della prima categoria
+     * @param nome_c2, nome della seconda categoria
+     * @param fattore, valore del fattore di conversione, dalle ore della prima categoria alle ore della seconda
+     */
+    public FattoreDiConversione(String nome_c1, String nome_c2, double fattore){
         this.nome_c1 = nome_c1;
         this.nome_c2 = nome_c2;
         this.fattore = fattore;
     }
-
-    
 
     public String getNome_c1() {
         return nome_c1;
@@ -28,11 +32,22 @@ public class FattoreDiConversione {
         this.nome_c2 = nome_c2;
     }
 
-    public float getFattore() {
+    public double getFattore() {
         return fattore;
     }
 
-    public void setFattore(float fattore) {
+    public void setFattore(double fattore) {
         this.fattore = fattore;
+    }
+
+    /**
+     * UTILIZZATO DAL GESTORE DI FATTORI.
+     * FARE ATTENZIONE SE SI MODIFICA
+     * TODO
+     * PREDISPORRE UN ALTRO "TOSTRING" PER IL GESTORE MAGARI FATTO AD HOC
+     * @return
+     */
+    public String toString(){
+        return  nome_c1 + " " + nome_c2 + " " + fattore;
     }
 }
--- src/it/unibs/projectIngesoft/attivita/GestoreFattori.java
+++ src/it/unibs/projectIngesoft/attivita/GestoreFattori.java
@@ -1,18 +1,107 @@
 package attivita;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GestoreFattori {
-    private String file_path;
+
+    private final String filePath;
     private HashMap<String, FattoreDiConversione> fattori;
 
-    public GestoreFattori(String file_path){
-        this.file_path = file_path;
+    public GestoreFattori(String filePath) {
+        this.filePath = filePath;
+        this.fattori = new HashMap<>();
         this.leggiFileFattori();
     }
 
-    private void leggiFileFattori(){
-        
+    public String getFilePath() {
+        return filePath;
     }
+
+    public HashMap<String, FattoreDiConversione> getFattori() {
+        return fattori;
+    }
+
+    public void setFattori(HashMap<String, FattoreDiConversione> fattori) {
+        this.fattori = fattori;
+    }
+
+    public void addFattore(String key, FattoreDiConversione value) {
+        this.fattori.put(key, value);
+    }
+
+    public boolean leggiFileFattori() {
+        File file;
+
+        HashMap<String, FattoreDiConversione> tempFattori = new HashMap<>();
+
+        try {
+            file = new File(this.filePath);
+            if (file.createNewFile())
+                System.out.println("FILE CREATO");
+            //sia che il file venga creato, sia che il file esista ritorno true => nessun problema
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+        //scrivo sul file la stringa formattata
+        try {
+            Scanner scanner = new Scanner(new File(this.filePath));
+
+            while (scanner.hasNext()) {
+                //System.out.println(scanner.nextLine());
+                String tempString = scanner.nextLine();
+                String[] words = tempString.split("\\W+");
+
+                FattoreDiConversione tempFattore = new FattoreDiConversione(words[0], words[1], Double.parseDouble(words[2]));
+                this.addFattore(words[0], tempFattore);
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @return true se ha scritto con successo. false altrimenti
+     */
+    public boolean scriviFileFattori() {
+
+        File file;
+        try {
+            file = new File(this.filePath);
+            if (file.createNewFile())
+                System.out.println("FILE CREATO");
+            //sia che il file venga creato, sia che il file esista ritorno true => nessun problema
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+        //scrivo sul file la stringa formattata
+        try (PrintWriter out = new PrintWriter(file)) {
+            for (FattoreDiConversione value : fattori.values()) {
+                out.println(value.toString());
+            }
+
+
+            System.out.println("FILE SCRITTO");
+            return true;
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
 }
