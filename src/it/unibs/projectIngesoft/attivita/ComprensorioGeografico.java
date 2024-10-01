faf4f15eef7ea6b36cbb0d381d84b4621b7fc7c7
kirbloz <one.baisini@gmail.com>
2024-10-01 11:34:50 +0200

    creata la classe per i comprensori geografici

---
+++ src/it/unibs/projectIngesoft/attivita/ComprensorioGeografico.java
@@ -0,0 +1,31 @@
+package attivita;
+
+import java.util.ArrayList;
+
+public class ComprensorioGeografico {
+    ArrayList<String> ListaComuni;
+
+    public ComprensorioGeografico(ArrayList<String> listaComuni) {
+        ListaComuni = listaComuni;
+    }
+
+    public ComprensorioGeografico() {
+        ListaComuni = new ArrayList<>();
+    }
+
+    public ArrayList<String> getListaComuni() {
+        return ListaComuni;
+    }
+
+    public boolean addComune(String comune){
+        return this.ListaComuni.add(comune);
+    }
+
+    public boolean removeComune(String comune){
+        return this.ListaComuni.remove(comune);
+    }
+
+    public void setListaComuni(ArrayList<String> listaComuni) {
+        ListaComuni = listaComuni;
+    }
+}
