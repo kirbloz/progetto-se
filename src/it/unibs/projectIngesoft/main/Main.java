3286e0b3a1f6d2127263bfe1f274487dcee82a71
martibarimaff <97847284+martibarimaff@users.noreply.github.com>
2024-10-03 11:10:31 +0200

    Menu per le Opzioni Creato nel Main

--- src/it/unibs/projectIngesoft/main/Main.java
+++ src/it/unibs/projectIngesoft/main/Main.java
@@ -1,12 +1,18 @@
 package main;
 
 import attivita.*;
+import it.unibs.fp.myutils.MyMenu;
 
 /* Classe main.Main.
 
  */
 public class Main {
     
+
+
+    private static final String TITOLO_MENU_CONFIGURAZIONE = "OPZIONI DI CONFIGURAZIONE";
+    private static final String[] OPZIONI_CONFIGURAZIONE = {"Cambio Password", "Crea Categoria", "Crea Comprensorio Geografico"};
+
     public static void main(String[] args) {
         // L'app si avvia con il login
         GestoreUtenti.login();
@@ -14,7 +20,9 @@ public class Main {
 
         //dopo il login come amministratore deve essere possibile scegliere che caso d'uso effettuare
         // (Classe per la gestione di questa scelta? o si mette tutto in configuratore?)
-
+        MyMenu opzioniConfigurazioneMenu = new MyMenu(TITOLO_MENU_CONFIGURAZIONE, OPZIONI_CONFIGURAZIONE);
+        opzioniConfigurazioneMenu.stampaMenu();
+        //DaFare: non mi ricordo come fare il case con MyMenu
     }
 }
 
