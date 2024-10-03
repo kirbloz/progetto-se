38777e3c0969ac3c9294441f5ad5040ad57e565a
martibarimaff <97847284+martibarimaff@users.noreply.github.com>
2024-10-03 15:31:11 +0200

    se non ho rotto tutto ho fixato qualcosa

--- src/it/unibs/projectIngesoft/attivita/GestoreUtenti.java
+++ src/it/unibs/projectIngesoft/attivita/GestoreUtenti.java
@@ -11,48 +11,46 @@ public class GestoreUtenti {
     public static final String MSG_RICHIESTA_USERNAME = "Inserisci il tuo username: ";
     public static final String MSG_RICHIESTA_PASSWORD = "Inserisci la tua password: ";
 
-    public static ArrayList<Utente> listaUtenti = new ArrayList<>(); //la lista degli utenti registrati (da salvare in fileCredenziali)
-    public static File fileCredenziali = new File("fileCredenziali"); //file contenente gli utenti registrati
-    
+    private static BloccoDiCredenziali insiemeCredenziali = new BloccoDiCredenziali();//la lista degli utenti registrati (da salvare in fileCredenziali)
+    public static File fileCredenziali = new File("fileCredenziali"); // file contenente gli utenti registrati
+
     /**
      * Serve per il login.
      */
-    public static void login(){
+    public static void login() {
         String username, password;
 
-        //da ciclare i guess -m
+        // da ciclare i guess -m
         username = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_USERNAME);
         password = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_PASSWORD);
 
-        if(username.equals(defaultAdminUsr) && password.equals(defaultAdminPsw)){
-            Configuratore C1 = new Configuratore(); //qui creiamo l'oggetto nuovo configuratore con nome C1, ma il nome deve cambiare per ogni istanza eventuale (o sono scemo?) (sono scemo -m)
+        if (username.equals(defaultAdminUsr) && password.equals(defaultAdminPsw)) {
+            Configuratore C1 = new Configuratore(); // qui creiamo l'oggetto nuovo configuratore con nome C1, ma il nome
+                                                    // deve cambiare per ogni istanza eventuale (o sono scemo?) (sono
+                                                    // scemo -m)
             C1.cambioCredenziali();
-            listaUtenti.add(C1);
+            BloccoDiCredenziali.listaUtenti.add(C1);
             scritturaCredenzialiSuFile();
-        }
-        else if(true/*nome inserito esiste && password corretta */){
-            //ok
-        }
-        else System.out.println("errore");//dai errore
+        } else if (true/* nome inserito esiste && password corretta */) {
+            // ok
+        } else
+            System.out.println("errore");// dai errore
     }
 
-    public void checkUtente(){  //Ricerca all'interno dell'ArrayList (dopo averlo popolato leggendo il fileCredenziali) l'esistenza della coppia username e password data in input
-    
+    public void checkUtente() { // Ricerca all'interno dell'ArrayList (dopo averlo popolato leggendo il
+                                // fileCredenziali) l'esistenza della coppia username e password data in input
+
     }
 
-    public static void scritturaCredenzialiSuFile(){   //Crea il fileCredenziali a partire dall'ArrayList listaUtenti
-        for (Utente listaUtenti : listaUtenti) {
-        ServizioFile.salvaSingoloOggetto(fileCredenziali, listaUtenti);
-        }
-    }
-
-    public static void letturaFileCredenziali(){      //Inserisce gli utenti e le password (che si trovano dentro al file) nell'ArrayList listaUtenti 
-        listaUtenti = (ArrayList<Utente>)ServizioFile.caricaSingoloOggetto(fileCredenziali);
-     for (Utente utente : listaUtenti) {
-        System.out.println(utente.getUsername() + utente.getPassword());
-     }
+    public static void scritturaCredenzialiSuFile() { // Crea il fileCredenziali a partire dall'ArrayList listaUtenti
         
+        ServizioFile.salvaSingoloOggetto(fileCredenziali, insiemeCredenziali);
     }
-    
+
+    public static void letturaFileCredenziali() { // Inserisce gli utenti e le password (che si trovano dentro al file)
+                                                  // nell'ArrayList listaUtenti
+        BloccoDiCredenziali blocco = (BloccoDiCredenziali)ServizioFile.caricaSingoloOggetto(fileCredenziali);
+        insiemeCredenziali.listaUtenti = blocco.listaUtenti;
+    }
 
 }
