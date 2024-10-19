742d85a59b1d3fd546aea17c6d3619a3ab3dbb4c
kirbloz <one.baisini@gmail.com>
2024-10-19 13:09:15 +0200

    format fix, introdotte costanti per i messaggi a video

--- src/main/java/it/unibs/projectIngesoft/main/Main.java
+++ src/main/java/it/unibs/projectIngesoft/main/Main.java
@@ -5,7 +5,6 @@ import it.unibs.projectIngesoft.gestori.GestoreCategorie;
 import it.unibs.projectIngesoft.gestori.GestoreFattori;
 import it.unibs.projectIngesoft.gestori.GestoreUtenti;
 import it.unibs.projectIngesoft.menu.Menu;
-import it.unibs.projectIngesoft.utente.Configuratore;
 import it.unibs.projectIngesoft.utente.Utente;
 
 import java.util.ArrayList;
@@ -16,11 +15,15 @@ import java.util.HashMap;
  */
 public class Main {
 
-    protected final static String filePath = "serialized.xml";
-    protected final static String nomeFileUtenti = "users.xml";
+    protected static final String FATTORI_DI_CONVERSIONE_XML_FILEPATH = "serialized.xml";
+    protected static final String UTENTI_XML_FILEPATH = "users.xml";
+    public static final String CATEGORIE_XML_FILEPATH = "categorie.xml";
 
-    protected final static String titoloMainMenu = "MENU' PRINCIPALE - SCAMBIO ORE";
-    protected final static String[] voci = new String[]{
+    public static final String MSG_PROGRAM_EXIT = "> ARRIVEDERCI <";
+    public static final String MSG_DEFAULT_NO_VALID_MENU_SELECTION = "Niente da mostrare.";
+
+    protected static final String TITLE_MAIN_MENU = "MENU' PRINCIPALE - SCAMBIO ORE";
+    protected static final String[] vociMain = new String[]{
             //"Accedi",
             "Cambia Credenziali",
             "Menu Comprensorio TODO",
@@ -28,8 +31,8 @@ public class Main {
             "Menu Fattori WIP"
     };
 
-    protected final static String titoloMenuCategorie = "MENU' CATEGORIE";
-    protected final static String[] vociCategorie = new String[]{
+    protected static final String TITLE_MENU_CATEGORIE = "MENU' CATEGORIE";
+    protected static final String[] vociCategorie = new String[]{
             "Aggiungi Categoria Non Foglia",
             "Aggiungi Categoria Foglia",
             "Aggiungi Gerarchia",
@@ -37,21 +40,21 @@ public class Main {
             "Visualizza Gerarchia"
     };
 
-    protected final static String titoloMenuFattori = "MENU' FATTORI";
-    protected final static String[] vociFattori = new String[]{
+    protected static final String TITLE_MENU_FATTORI = "MENU' FATTORI";
+    protected static final String[] vociFattori = new String[]{
             "Visualizza Fattori di Conversione"
-            // TODO
     };
 
-    protected final static ArrayList<FattoreDiConversione> listaFattori = new ArrayList<>() {{
+    // TODO remove this test data
+    protected static final ArrayList<FattoreDiConversione> listaFattori = new ArrayList<>() {{
         add(new FattoreDiConversione("c1", "c2", 4.5));
         add(new FattoreDiConversione("c1", "c3", 0.2));
     }};
 
-
-    protected final static HashMap<String, ArrayList<FattoreDiConversione>> mapFattori = new HashMap<>() {{
+    protected static final HashMap<String, ArrayList<FattoreDiConversione>> mapFattori = new HashMap<>() {{
         put("c1", listaFattori);
     }};
+
 
     protected static Utente utenteLoggato = new Utente();
 
@@ -60,10 +63,10 @@ public class Main {
         /*
             Creazione di uno pseudo menu fittizio per testare i casi d'uso durante l'implementazione.
          */
-        GestoreUtenti userHandler = new GestoreUtenti(nomeFileUtenti);
-        Menu menu = new Menu(titoloMainMenu, voci);
-        Menu menuCategorie = new Menu(titoloMenuCategorie, vociCategorie);
-        Menu menuFattori = new Menu(titoloMenuCategorie, vociFattori);
+        GestoreUtenti userHandler = new GestoreUtenti(UTENTI_XML_FILEPATH);
+        Menu menu = new Menu(TITLE_MAIN_MENU, vociMain);
+        Menu menuCategorie = new Menu(TITLE_MENU_CATEGORIE, vociCategorie);
+        Menu menuFattori = new Menu(TITLE_MENU_CATEGORIE, vociFattori);
 
 
         utenteLoggato = userHandler.login();
@@ -95,21 +98,28 @@ public class Main {
             scelta = menu.scegli();
 
             switch (scelta) {
-                //TODO
+                case 0:
+                    System.out.println(MSG_PROGRAM_EXIT);
+                    break;
                 case 1:
                     // cambio credenzilai
                     userHandler.cambioCredenziali(utenteLoggato);
                     break;
-                case 4:
+                case 2:
+                    //menu comprensorio
+                    // TODO
+                    // @martino
+                    break;
+                case 3:
                     //menu categorie
                     loopCategorie(menuCategorie);
                     break;
-                case 5:
+                case 4:
                     //menu fattori
                     loopFattori(menuFattori);
                     break;
                 default:
-                    System.out.println("Niente da mostrare.");
+                    System.out.println(MSG_DEFAULT_NO_VALID_MENU_SELECTION);
             }
 
         } while (scelta != 0);
@@ -122,7 +132,7 @@ public class Main {
      */
     private static void loopCategorie(Menu menuCategorie) {
         int scelta = 0;
-        GestoreCategorie gestCat = new GestoreCategorie("categorie.xml");
+        GestoreCategorie gestCat = new GestoreCategorie(CATEGORIE_XML_FILEPATH);
 
         do {
             scelta = menuCategorie.scegli();
@@ -137,7 +147,7 @@ public class Main {
      */
     private static void loopFattori(Menu menuFattori) {
         int scelta = 0;
-        GestoreFattori gestFatt = new GestoreFattori(filePath);
+        GestoreFattori gestFatt = new GestoreFattori(FATTORI_DI_CONVERSIONE_XML_FILEPATH);
 
         //per hot testing
         //gestFatt.setFattori(mapFattori);
