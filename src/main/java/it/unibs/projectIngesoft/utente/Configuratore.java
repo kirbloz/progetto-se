c44910549127ba3eef73fd10e3fcb5f93416b86d
W1L7dev <williammiclette@icloud.com>
2024-10-28 13:59:57 +0100

    Fix errori di Kirbloz (come sempre)

--- src/main/java/it/unibs/projectIngesoft/utente/Configuratore.java
+++ src/main/java/it/unibs/projectIngesoft/utente/Configuratore.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 public class Configuratore extends Utente{
 
     public Configuratore() {
+        this.username = "admin";
+        this.password = "1234";
     }
 
     public Configuratore(String username, String password) {       // Caso utente //??? a cosa serve il costruttore con username e password? -m
