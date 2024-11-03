e64ef300a6fa3e42b82153b89cfef02d115ccc9d
kirbloz <one.baisini@gmail.com>
2024-11-03 21:58:33 +0100

    implementazione della memorizzazione su file delle credenziali default (ora non più hardcoded!!); fix javadoc, format code; semplificato login(), ricercaUtente() e creato il nuovo metodo cercaUtenteConUsername()

--- src/main/java/it/unibs/projectIngesoft/gestori/GestoreUtenti.java
+++ src/main/java/it/unibs/projectIngesoft/gestori/GestoreUtenti.java
@@ -1,7 +1,7 @@
 package it.unibs.projectIngesoft.gestori;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import it.unibs.fp.myutils.InputDati;
+import it.unibs.projectIngesoft.libraries.InputDati;
 import it.unibs.projectIngesoft.libraries.Serializer;
 import it.unibs.projectIngesoft.utente.Configuratore;
 import it.unibs.projectIngesoft.utente.Utente;
@@ -11,42 +11,59 @@ import java.util.List;
 
 public class GestoreUtenti {
 
-    // da levare l'hardcoding
-    public static final String defaultAdminUsr = "admin";
-    public static final String defaultAdminPsw = "1234";
-
     public static final String MSG_RICHIESTA_USERNAME = "Inserisci il tuo username: ";
     public static final String MSG_RICHIESTA_PASSWORD = "Inserisci la tua password: ";
 
-    public static final String MSG_RICHIESTA_NUSERNAME = "Inserisci il nuovo username: "; //per cambio username
-    public static final String MSG_RICHIESTA_NPASSWORD = "Inserisci la nuova password: "; //per cambio password
+    public static final String MSG_RICHIESTA_NEW_USERNAME = "Inserisci il nuovo username: "; //per cambio username
+    public static final String MSG_RICHIESTA_NEW_PASSWORD = "Inserisci la nuova password: "; //per cambio password
 
     private final String filePath;
+    private final String defaultCredentialsFilePath;
     private ArrayList<Utente> utenti;
+    private Utente defaultUtente;
 
-    public GestoreUtenti(String filePath) {
+    public GestoreUtenti(String filePath, String defaultCredentialsFilePath) {
         this.filePath = filePath;
+        this.defaultCredentialsFilePath = defaultCredentialsFilePath;
         this.utenti = new ArrayList<>();
         deserializeXML(); // load dati
     }
 
-    public String getFilePath() {
-        return filePath;
-    }
-
-    public ArrayList<Utente> getUtenti() {
-        return utenti;
-    }
-
-    public void setUtenti(ArrayList<Utente> utenti) {
-        this.utenti = utenti;
-        serializeXML();
+    /**
+     * Sfrutto l'implementazione statica della classe Serializer.
+     * Questo metodo esiste da prima di Serializer, per non cambiare ogni call a questo ho semplicemente sostituito il corpo.
+     */
+    public void serializeXML() {
+        assert this.utenti != null;
+        assert this.filePath != null;
+        Serializer.serialize(this.filePath, this.utenti);
     }
 
     /**
-     * inserisce l'utente nell'arraylist se e solo se non esiste già
+     * Sfrutto l'implementazione statica della classe Serializer.
+     * Questo metodo esiste da prima di Serializer, per non cambiare ogni call a questo ho semplicemente sostituito il corpo.
+     */
+    public void deserializeXML() {
+        assert this.filePath != null;
+        assert this.defaultCredentialsFilePath != null;
+
+        List<Utente> listaUtenti = Serializer.deserialize(new TypeReference<>() {
+        }, this.filePath);
+        this.utenti.clear();
+        if (listaUtenti != null)
+            this.utenti.addAll(listaUtenti);
+
+        this.defaultUtente = Serializer.deserialize(new TypeReference<>() {
+        }, this.defaultCredentialsFilePath);
+
+        assert this.utenti != null : "deve essere almeno inizializzato!";
+        assert this.defaultUtente != null : "l'utente default non può essere null, deve esistere";
+    }
+
+    /**
+     * Inserisce l'utente nell'attributo utenti se e solo se non esiste già
      *
-     * @param utente
+     * @param utente, utente da inserire
      */
     public void addUtente(Utente utente) {
         if (!this.utenti.contains(utente)) {
@@ -62,115 +79,73 @@ public class GestoreUtenti {
         String username;
         String password;
         int indice;
-        // TODO
-        // spostare le credenziali da hardcoding a un file credenzialiDefault.xml
-
         do {
-            // Richiesta Input per il login
+            // immissione credenziali per il login
             username = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_USERNAME);
             password = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_PASSWORD);
-
             // controllo se credenziali default e in caso creo il nuovo configuratore
-            if (username.equals(defaultAdminUsr) && password.equals(defaultAdminPsw)) {
+            if (username.equals(defaultUtente.getUsername()) && password.equals(defaultUtente.getPassword())) {
                 Configuratore C1 = new Configuratore();
-                this.cambioCredenziali(C1);
+                cambioCredenziali(C1);
                 addUtente(C1);
                 return C1;
             }
-
             // ricerca
             indice = ricercaUtente(username, password);
             if (indice == -1)
                 System.out.println("Credenziali errate.");
-
         } while (indice == -1);
 
         return utenti.get(indice);
     }
 
     /**
-     * effettua il controllo sulle nuove credenziali (per mantenere l'unicità dell'username) e poi chiama la funzione dell'utente per modificarle
+     * Effettua il controllo sulle nuove credenziali (per mantenere l'unicità dello username)
+     * e poi chiama la funzione dell'utente per modificarle.
      *
-     * @param C1
+     * @param C1, utente di cui cambiare le credenziali
      */
     public void cambioCredenziali(Utente C1) {
-        boolean exists = false;
         String newUsername;
         String newPassword;
-        String oldUsername = C1.getUsername();
-
         do {
-            newUsername = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_NUSERNAME);
-            newPassword = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_NPASSWORD);
-            if (!(oldUsername == null)) {
-                if (!oldUsername.equals(newUsername)) {
-                    for (Utente usr : this.utenti) {
-                        if (usr.getUsername().equals(newUsername)) {
-                            exists = true;
-                            System.out.println("Nome utente gia' esistente");
-                            break;
-                        }
-                        exists = false; // Nel caso si entrasse nell'if sopra almeno una volta
-                    }
-                }
-            }
-        } while (exists);
-
+            newUsername = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_NEW_USERNAME);
+            newPassword = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_NEW_PASSWORD);
+        } while (cercaConUsername(newUsername) != null);
         C1.cambioCredenziali(newUsername, newPassword);
         serializeXML();
     }
 
     /**
-     * Ricerca all'interno dell'ArrayList (dopo averlo popolato leggendo il
-     * fileCredenziali) l'esistenza della coppia username e password data in input
+     * Verifica se lo username esiste già.
      *
-     * @param username
-     * @param password
+     * @param username lo username da verificare
+     * @return oggetto utente corrispondente se lo username esiste, null altrimenti
+     */
+    private Utente cercaConUsername(String username) {
+        assert this.utenti != null;
+        return this.utenti.stream()
+                .filter(usr -> usr.getUsername().equals(username))
+                .findFirst()
+                .orElse(null);
+    }
+
+    /**
+     * Ricerca all'interno dell'attributo utenti 'esistenza della coppia username e password data in ingresso
+     *
+     * @param username, username dell'utente cercato
+     * @param password, password dell'utente cercato
      * @return int indice dell'oggetto, altrimenti "-1" se
      */
     public int ricercaUtente(String username, String password) {
-        for (Utente utente : utenti) {
-            if (utente.getUsername().equals(username) && utente.getPassword().equals(password)) {
-                return utenti.indexOf(utente);
-            }
+        Utente tempUtente = cercaConUsername(username);
+        if (tempUtente == null) // se l'utente non esiste, ritorna -1
+            return -1;
+        // se l'utente esiste allora controllo che la password sia quella corretta
+        if (tempUtente.getPassword().equals(password)) {
+            return utenti.indexOf(tempUtente);
         }
+        // se la password è errata, ritorna -1
         return -1;
     }
-
-    /**
-     * Sfrutto l'implementazione statica della classe Serializer.
-     * Questo metodo esiste da prima di Serializer, per non cambiare ogni call a questo ho semplicemente sostituito il corpo.
-     */
-    public void serializeXML() {
-
-        assert this.utenti != null;
-        assert this.filePath != null;
-
-        Serializer.serialize(this.filePath, this.utenti);
-
-    }
-
-    /**
-     * Sfrutto l'implementazione statica della classe Serializer.
-     * Questo metodo esiste da prima di Serializer, per non cambiare ogni call a questo ho semplicemente sostituito il corpo.
-     */
-    public void deserializeXML() {
-        assert this.filePath != null;
-
-        List<Utente> listaUtenti = Serializer.deserialize(new TypeReference<>() {
-        }, this.filePath);
-
-        /* for debugging */
-        for (Utente obj : listaUtenti) {
-            System.out.println(obj.toString());
-        }
-
-        this.utenti.clear();
-        if (listaUtenti != null)
-            this.utenti.addAll(listaUtenti);
-
-        assert this.utenti != null : "deve essere almeno inizializzato!";
-    }
-
-
 }
