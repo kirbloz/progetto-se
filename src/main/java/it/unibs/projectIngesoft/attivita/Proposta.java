f7ddeae3ad76de9547299b9d7152dd1695b2dad7
kirbloz <one.baisini@gmail.com>
2024-11-06 21:21:28 +0100

    preparato per la serializzazione

--- src/main/java/it/unibs/projectIngesoft/attivita/Proposta.java
+++ src/main/java/it/unibs/projectIngesoft/attivita/Proposta.java
@@ -1,10 +1,23 @@
 package it.unibs.projectIngesoft.attivita;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JacksonXmlRootElement(localName = "Proposta")
 public class Proposta {
+
+    @JacksonXmlProperty(localName = "richiesta")
     private String richiesta;
+    @JacksonXmlProperty(localName = "offerta")
     private String offerta;
+    @JacksonXmlProperty(localName = "oreRichiesta")
     private int oreRichiesta;
+    @JacksonXmlProperty(localName = "oreOfferta")
     private int oreOfferta;
+
+    public Proposta(){
+
+    }
 
     public Proposta(String richiesta, String offerta, int oreRichiesta, int oreOfferta) {
         this.richiesta = richiesta;
