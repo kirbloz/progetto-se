ce325bfe62bfaa3a2bcda60bb1bfbab200be6119
martibarimaff <97847284+martibarimaff@users.noreply.github.com>
2024-11-06 19:06:57 +0100

    Il gestoreProposte dovrebbe essere operativo

--- src/main/java/it/unibs/projectIngesoft/gestori/GestoreProposte.java
+++ src/main/java/it/unibs/projectIngesoft/gestori/GestoreProposte.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 public class GestoreProposte {
     public static final String MSG_RICHIESTA_RADICE = "Inserisci la radice: ";
     private static final String MSG_RICHIESTA_FOGLIA = "Inserisci la foglia: ";
+    public static final String MSG_INSERISCI_RICHIESTA = ">> Inserisci una categoria valida di cui vuoi effettuare la richiesta.";
+    public static final String MSG_INSERISCI_OFFERTA = ">> Inserisci una categoria valida che sei disposto a offrire in cambio.";
+    public static final String MSG_RICHIESTA_ORE = "Inserisci il numero di ore che vuoi richiedere: ";
 
     private ArrayList<Proposta> listaProposte;
     private GestoreFattori gestFatt;
@@ -21,31 +24,38 @@ public class GestoreProposte {
         String richiesta, offerta;
         int oreRichiesta, oreOfferta;
 
-        richiesta = richiestaFoglia();//TODO controllo esistenza foglia
+
+        richiesta = richiestaFoglia();
+        oreRichiesta = InputDati.leggiInteroPositivo(MSG_RICHIESTA_ORE);
+
         offerta = richiestaFoglia();
-        oreRichiesta = InputDati.leggiInteroPositivo("Inserisci il numero di ore richieste: ");
         try {
             oreOfferta = gestFatt.calcolaRapportoOre(richiesta, offerta, oreRichiesta);
         } catch (Exception e) {
+            System.out.println("Impossibile Calcolare il numero di ore da offrire");
             throw new RuntimeException(e); //TODO guarda di fare l'eccezione giusta
         }
 
-
-        addProposta(new Proposta(richiesta, offerta, oreRichiesta, oreOfferta));
+        if (InputDati.yesOrNo("Dovrai offrire %d ore in cambio, confermi? S/N".formatted(oreOfferta))){
+            addProposta(new Proposta(richiesta, offerta, oreRichiesta, oreOfferta));
+        } else System.out.println("Proposta Annullata");
 
     }
 
-    public String richiestaFoglia(){  //TODO chiedere a wade se usare categorie o fattori di conversione per controllo
-    String radice, foglia;          //TODO chiedere a wade se ha senso spostare factorNameBuilder fuori da gestoreFattori
-        radice = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_RADICE);
-        foglia = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_FOGLIA);
+    public String richiestaFoglia(){
+    String radice, foglia, categoria;
 
-        return gestFatt.factorNameBuilder(radice, foglia);
+        do {
+            System.out.println(MSG_INSERISCI_RICHIESTA);
+            radice = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_RADICE);
+            foglia = InputDati.leggiStringaNonVuota(MSG_RICHIESTA_FOGLIA);
+            categoria = gestFatt.factorNameBuilder(radice, foglia);
+        } while (gestFatt.hashmapContainsKey(categoria));
+
+        return categoria;
     }
 
-
-
-    public void addProposta(Proposta proposta){    //TODO
-
+    public void addProposta(Proposta proposta){
+            listaProposte.add(proposta);
     }
 }
