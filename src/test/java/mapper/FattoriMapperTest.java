7e0275b10fef3a1e7a1477d1465848894434064f
kirbloz <one.baisini@gmail.com>
2025-01-28 20:48:28 +0100

    convertito i dati xml dei fattori di conversione in json con un (non) semplice e pratico serializer ad hoc

--- src/test/java/mapper/FattoriMapperTest.java
+++ src/test/java/mapper/FattoriMapperTest.java
@@ -1,22 +1,31 @@
 package mapper;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import it.unibs.projectIngesoft.attivita.FattoreDiConversione;
 import it.unibs.projectIngesoft.gestori.FattoriModel;
 import it.unibs.projectIngesoft.mappers.FattoriMapper;
+import it.unibs.projectIngesoft.parsing.JacksonSerializer;
 import it.unibs.projectIngesoft.parsing.SerializerJSON;
+import it.unibs.projectIngesoft.parsing.SerializerXML;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FattoriMapperTest {
 
     FattoriModel model;
+    FattoriMapper mapper;
 
     @BeforeEach
     void prepareTests(){
-        FattoriMapper mapper = new FattoriMapper("fattoriTest.json",
+        this.mapper = new FattoriMapper("fattoriTest.json",
                 new SerializerJSON<>());
         model = new FattoriModel(mapper);
 
@@ -42,4 +51,23 @@ public class FattoriMapperTest {
         assert !model.getHashListaFattori().get("catUno").isEmpty();
 
     }
+
+    @Test
+    void readXML_andWriteJSON(){
+        SerializerXML<FattoriWrapper> serializer = new SerializerXML();
+        FattoriWrapper readData = serializer.deserialize(new TypeReference<>(){} , "fattori.xml");
+        Map<String, List<FattoreDiConversione>> hashmpa = readData.toHashMap();
+        System.out.println(hashmpa);
+        for(String cat : readData.toHashMap().keySet()){
+            System.out.println(cat);
+            System.out.println(hashmpa.get(cat));
+        }
+        assert hashmpa != null && !hashmpa.isEmpty();
+
+
+        mapper.write(hashmpa);
+    }
+
+
+
 }
---
+++ src/test/java/mapper/FattoriWrapper.java
@@ -0,0 +1,99 @@
+package mapper;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import it.unibs.projectIngesoft.attivita.FattoreDiConversione;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@JacksonXmlRootElement(localName = "FattoriWrapper")
+public class FattoriWrapper {
+
+    // annotazione per racchiudere la lista "fattori" in un solo tag XML
+    @JacksonXmlElementWrapper(localName = "fattori")
+    // annotazione per racchiudere ogni elemento della lista in una tag "entry"
+    @JacksonXmlProperty(localName = "entry")
+    private List<Entry> fattori;
+
+    /**
+     * Costruttore di default necessario per la deserializzazione di Jackson
+     */
+    public FattoriWrapper() {
+        this.fattori = new ArrayList<>();
+    }
+
+    /**
+     * Costruttore con parametro per inizializzare e popolare l'attributo fattori.
+     * Converte ogni entry della HashMap in ingresso in un oggetto Entry e lo aggiunge alla lista
+     * attributo fattori.
+     *
+     * @param map, mappa per costruire l'attributo fattori
+     */
+    public FattoriWrapper(Map<String, List<FattoreDiConversione>> map) {
+        this.fattori = new ArrayList<>();
+        for (Map.Entry<String, List<FattoreDiConversione>> entry : map.entrySet()) {
+            this.fattori.add(new Entry(entry.getKey(), entry.getValue()));
+        }
+    }
+
+    /**
+     * Converte la lista di Entry in una HashMap.
+     * Serve durante la deserializzazione: per ogni Entry della lista, si estraggono la chiave
+     * e il valore tramite split, poi li si inserisce nella Map che si sta costruendo
+     *
+     * @return map, HashMap costruita
+     */
+    public HashMap<String, List<FattoreDiConversione>> toHashMap() {
+        HashMap<String, List<FattoreDiConversione>> map = new HashMap<>();
+        for (Entry entry : this.fattori) {
+            String keyParsed = entry.getKey();
+            map.put(keyParsed, entry.getValue());
+        }
+        return map;
+    }
+
+    /**
+     * Classe interna, statica per rappresentare un singolo elemento della mappa.
+     */
+    public static class Entry {
+
+        // Annotazione per specificare il nome del tag XML per la chiave
+        @JacksonXmlProperty(localName = "key")
+        private String key;
+
+        // annotazione per racchiudere la lista 'values' in un singolo tag XML
+        @JacksonXmlElementWrapper(localName = "values")
+        // annotazione per specificare il nome del tag per ogni valore nella lista
+        @JacksonXmlProperty(localName = "value")
+        private List<FattoreDiConversione> value;
+
+        /**
+         * Costruttore default necessario per la deserializzazione di Jackson
+         */
+        public Entry() {
+        }
+
+        /**
+         * Costruttore con parametri per inizializzare e impostare gli attributi.
+         * @param key, chiave
+         * @param value, valore
+         */
+        public Entry(String key, List<FattoreDiConversione> value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public List<FattoreDiConversione> getValue() {
+            return value;
+        }
+    }
+}
+
