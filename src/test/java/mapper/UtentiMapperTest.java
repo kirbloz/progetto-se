88d8bd7a0a2b686596d52479760df763e8f31203
kirbloz <one.baisini@gmail.com>
2025-01-29 19:54:39 +0100

    format

--- src/test/java/mapper/UtentiMapperTest.java
+++ src/test/java/mapper/UtentiMapperTest.java
@@ -1,16 +1,18 @@
 package mapper;
 
+import it.unibs.projectIngesoft.libraries.InputDatiTerminale;
+import it.unibs.projectIngesoft.mappers.UtentiMapper;
 import it.unibs.projectIngesoft.parsing.SerializerJSON;
 import it.unibs.projectIngesoft.parsing.SerializerXML;
-import it.unibs.projectIngesoft.mappers.UtentiMapper;
 import it.unibs.projectIngesoft.utente.Configuratore;
 import it.unibs.projectIngesoft.utente.Fruitore;
 import it.unibs.projectIngesoft.utente.Utente;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
-
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +20,10 @@ class UtentiMapperTest {
 
     private UtentiMapper mapper;
 
+
+
     @BeforeEach
-    void prepareTest(){
+    void prepareTest() {
         this.mapper = new UtentiMapper("usersTest.json",
                 "defaultCredentials.json",
                 new SerializerJSON<List<Utente>>(),
@@ -34,17 +38,19 @@ class UtentiMapperTest {
         //ObjectMapper objectMapper = new ObjectMapper();
     }
 
+
+
     @Test
     void writeTest() throws IOException {
         // Creazione di una lista di esempio di oggetti Utente
         List<Utente> utenti = new ArrayList<>();
-                utenti.add(new Configuratore("tizianoFerro", "LoStadio2015"));
-                utenti.add(new Configuratore("robertoBenigni", "LaVitaèBella97"));
-                utenti.add(new Fruitore("Gabbayyyy", "wasd1234", "gabriele.baiguini@gmail.com", "Bergamo"));
-                utenti.add(new Fruitore("martinomaff", "martino123", "martino.bariselli@gmail.com", "Brescia"));
-                utenti.add(new Fruitore("WadeGiovanniBaiguini", "fourmis7", "wade.baiguini@gmail.com", "Brescia"));
-                utenti.add(new Fruitore("LucaMaff", "luca1234", "luca.barismaff@gmail.com", "Brescia"));
-                utenti.add(new Fruitore("GiorgioVanni", "gormiti1", "giorgio.baigua@tiscali.it", "Bergamo"));
+        utenti.add(new Configuratore("tizianoFerro", "LoStadio2015"));
+        utenti.add(new Configuratore("robertoBenigni", "LaVitaèBella97"));
+        utenti.add(new Fruitore("Gabbayyyy", "wasd1234", "gabriele.baiguini@gmail.com", "Bergamo"));
+        utenti.add(new Fruitore("martinomaff", "martino123", "martino.bariselli@gmail.com", "Brescia"));
+        utenti.add(new Fruitore("WadeGiovanniBaiguini", "fourmis7", "wade.baiguini@gmail.com", "Brescia"));
+        utenti.add(new Fruitore("LucaMaff", "luca1234", "luca.barismaff@gmail.com", "Brescia"));
+        utenti.add(new Fruitore("GiorgioVanni", "gormiti1", "giorgio.baigua@tiscali.it", "Bergamo"));
 
         // Serializzazione in formato JSON
         //objectMapper.writerFor(type).writeValue(new File("utentiTest.json"), utenti);
@@ -58,14 +64,15 @@ class UtentiMapperTest {
     void readTest() throws IOException {
         List<Utente> utenti = mapper.read();
         //JSONMapper mapper2 = new JSONMapper("usersTest.json");
-       //List<Utente> utenti = mapper2.readList(Utente.class);
+        //List<Utente> utenti = mapper2.readList(Utente.class);
 
-        for(Utente utente : utenti){System.out.println(utente);}
+        for (Utente utente : utenti) {
+            System.out.println(utente);
+        }
 
         assert utenti != null;
         assert utenti.get(0).getUsername().equals("tizianoFerro");
     }
-
 
 
     @Test
@@ -78,4 +85,9 @@ class UtentiMapperTest {
         System.out.println(def);
         assert def.getUsername().equals("admin");
     }
+
+
+
+
+
 }
