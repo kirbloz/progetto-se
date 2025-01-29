2b84fabe73f952d7a0b204452449a58ba6e0331d
kirbloz <one.baisini@gmail.com>
2025-01-29 19:54:24 +0100

    implementato un meccanismo per iniettare dati di test direttamente in System.in

--- src/test/java/utente/FruitoreTest.java
+++ src/test/java/utente/FruitoreTest.java
@@ -2,19 +2,19 @@ package utente;
 
 import it.unibs.projectIngesoft.attivita.ComprensorioGeografico;
 import it.unibs.projectIngesoft.gestori.UtentiModel;
+import it.unibs.projectIngesoft.libraries.InputDatiTerminale;
 import it.unibs.projectIngesoft.mappers.UtentiMapper;
-import it.unibs.projectIngesoft.parsing.JacksonSerializer;
 import it.unibs.projectIngesoft.parsing.SerializerJSON;
-import it.unibs.projectIngesoft.parsing.SerializerXML;
-import it.unibs.projectIngesoft.utente.*;
-import org.junit.jupiter.api.*;
+import it.unibs.projectIngesoft.utente.Fruitore;
+import it.unibs.projectIngesoft.utente.Utente;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FruitoreTest {
 
@@ -22,35 +22,74 @@ class FruitoreTest {
     private Utente fruitore;
     private UtentiModel model;
 
-    @BeforeEach
-    void prepareTest(){
+    private UtentiMapper mapper;
+    private List<Utente> cleanTestData;
 
-        UtentiMapper mapper = new UtentiMapper("users.json",
+    private final static InputStream systemIn = System.in;
+    private ByteArrayInputStream typeIn;
+
+    @BeforeEach
+    void prepareTest() {
+
+        mapper = new UtentiMapper("usersTest.json",
                 "defaultCredentials.json",
                 new SerializerJSON<List<Utente>>(),
                 new SerializerJSON<Utente>()
-                );
+        );
+
+        cleanTestData = new ArrayList<>();
+        cleanTestData = mapper.read();
+
         this.model = new UtentiModel(mapper);
 
         this.comprensorio = new ComprensorioGeografico("comprensorio", List.of("comune1"));
         this.fruitore = new Fruitore("user", "pwd", "valid@email.address", "comprensorio");
     }
 
-    //@AfterEach
-    @Test
-    void cleanTest(){
 
-           /*String input = "5";
-            ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
-            System.setIn(in);  // Redirect System.in to use our simulated input
-            // Assert that the square of 5 is 25
-            assertEquals(5, Integer.parseInt(input));
-
-*/
-        System.setIn(System.in);
+    @AfterEach
+    void tearDown() {
+        System.setIn(systemIn);
+        mapper.write(cleanTestData);
+        //System.setOut(systemOut);
     }
 
-    void primoAccessoFruitore_Registrazione(){
+    void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    @Test
+    void testInputInjectionSystemIN() {
+
+        //String user = InputDatiTerminale.leggiStringa(">> INPUT: ");
+
+        /*InputStream systemIn = System.in;
+        PrintStream systemOut = System.out;
+        ByteArrayInputStream typeIn;
+        ByteArrayOutputStream typeOut;*/
+
+        // set up
+        /*typeOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(typeOut));*/
+
+        String simulatedUserInput = "1";
+
+        provideInput(simulatedUserInput);
+        //System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+
+        int result = InputDatiTerminale.leggiIntero("");
+
+        assert result == 1;
+
+        // tear down
+        System.setIn(systemIn);
+        //System.setOut(systemOut);
+
+
+    }
+
+    void primoAccessoFruitore_Registrazione() {
         // todo
         // qui c'è il problema di avere uno stub che simuli il check del "database utenti"
         // si attende di fare il refactoring che astragga il meccanismo di serializzazione xml
@@ -60,24 +99,32 @@ class FruitoreTest {
     void cambioCredenzialiFruitore_UsernameEPassword() throws Exception {
 
         //check funzione interna
-        model.addUtente(fruitore);
-        if(model.ricercaUtente("user", "pwd") >= 0)
+        /*model.addUtente(fruitore);
+        if (model.ricercaUtente("user", "pwd") >= 0)
             fruitore.cambioCredenziali("newUser", "newPwd");
         assert fruitore.getUsername().equals("newUser");
         assert fruitore.getPassword().equals("newPwd");
         //check check esterno
-        assert null != model.verificaCredenziali(new String[]{"newUser", "newPwd"});
+        assert null != model.verificaCredenziali(new String[]{"newUser", "newPwd"});*/
+
+        model.addUtente(fruitore);
+        //System.out.println(UtentiModel.getListaUtenti());
+        provideInput("fruitore\npwd1\n");
+        model.cambioCredenziali(fruitore);
+
+        assert fruitore.getUsername().equals("fruitore")
+                && fruitore.getPassword().equals("pwd1");
     }
 
     @Test
-    void cambioCredenzialiFruitore_SoloPassword(){
+    void cambioCredenzialiFruitore_SoloPassword() {
         fruitore.cambioCredenziali("user", "newPwd");
         assert fruitore.getUsername().equals("user");
         assert fruitore.getPassword().equals("newPwd");
     }
 
     @Test
-    void cambioCredenzialiFruitore_SoloUsername(){
+    void cambioCredenzialiFruitore_SoloUsername() {
         fruitore.cambioCredenziali("newUser", "pwd");
         assert fruitore.getUsername().equals("newUser");
         assert fruitore.getPassword().equals("pwd");
@@ -85,9 +132,9 @@ class FruitoreTest {
 
 
     @Test
-    void cambioCredenzialiFruitore_SoloUsername_EsisteGia(){
+    void cambioCredenzialiFruitore_SoloUsername_EsisteGia() {
 
-        String newUsername="tizianoFerro";
+        String newUsername = "tizianoFerro";
 
         assert model.existsUsername(newUsername);
 
@@ -100,8 +147,6 @@ class FruitoreTest {
         assert fruitore.getUsername().equals("newUser");
         assert fruitore.getPassword().equals("pwd");*/
     }
-
-
 
 
 }
