d7d1a9017828a115576d9f44d985afa9f4a5e347
kirbloz <one.baisini@gmail.com>
2025-02-03 18:01:23 +0100

    ORA RUNNA

--- src/test/java/utente/ConfiguratoreTest.java
+++ src/test/java/utente/ConfiguratoreTest.java
@@ -1,13 +1,13 @@
 package utente;
 
 import it.unibs.projectIngesoft.controller.AccessoController;
+import it.unibs.projectIngesoft.controller.ConfiguratoreController;
 import it.unibs.projectIngesoft.libraries.InputInjector;
-import it.unibs.projectIngesoft.mappers.CompGeoMapper;
-import it.unibs.projectIngesoft.model.ComprensorioGeograficoModel;
 import it.unibs.projectIngesoft.model.UtentiModel;
 import it.unibs.projectIngesoft.parsing.SerializerJSON;
 import it.unibs.projectIngesoft.mappers.UtentiMapper;
 import it.unibs.projectIngesoft.utente.Configuratore;
+import it.unibs.projectIngesoft.view.ConfiguratoreView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,16 +15,15 @@ public class ConfiguratoreTest {
 
     private UtentiModel model;
     private Configuratore configuratore;
-    private UtentiMapper reader;
 
     @BeforeEach
     void prepareTest(){
-        this.reader = new UtentiMapper("usersTest.json",
+        UtentiMapper mapper = new UtentiMapper("usersJSONTEST.json",
                 "defaultCredentials.json",
                 new SerializerJSON<>(),
                 new SerializerJSON<>());
         this.configuratore = new Configuratore("admin", "pwd");
-        this.model = new UtentiModel(this.reader);
+        this.model = new UtentiModel(mapper);
     }
 
     @Test
@@ -35,6 +34,26 @@ public class ConfiguratoreTest {
 
         Configuratore utenteAttivo = (Configuratore) accessoController.login();
         assert utenteAttivo.firstAccess();
+    }
+
+    @Test
+    void primoAccessoConfiguratore_E_CambioPassword() {
+        Configuratore utenteAttivo = new Configuratore("admin", "1234");
+        utenteAttivo.setFirstAccess(true);
+
+        ConfiguratoreController controller = new ConfiguratoreController(
+        new ConfiguratoreView(),
+        null,
+        null,
+        null,
+                null,
+                model,
+                utenteAttivo
+        );
+        InputInjector.inject("unique\npassword\n0");
+        controller.run();
+
+        assert !utenteAttivo.firstAccess();
     }
 
     @Test
@@ -58,4 +77,6 @@ public class ConfiguratoreTest {
         assert configuratore.getUsername().equals("newAdmin");
         assert configuratore.getPassword().equals("pwd");
     }
+
+
 }
