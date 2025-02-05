97448228091880a3abf6f55ea0a7980339148639
kirbloz <one.baisini@gmail.com>
2025-02-05 10:52:22 +0100

    implementata la viewFactory (mi ero dimenticato di pushare questo file)

---
+++ src/main/java/it/unibs/projectIngesoft/view/ViewFactory.java
@@ -0,0 +1,15 @@
+package it.unibs.projectIngesoft.view;
+
+import it.unibs.projectIngesoft.utente.Utente;
+
+public class ViewFactory {
+    public static ErmesTerminaleView createView(Utente utente) {
+        String nomeClasse = "it.unibs.projectIngesoft.view." + utente.getClass().getSimpleName() + "View";
+        try {
+            Class<? extends ErmesTerminaleView> viewClass = Class.forName(nomeClasse).asSubclass(ErmesTerminaleView.class);
+            return viewClass.getDeclaredConstructor().newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Unable to create view for user type: " + utente.getClass().getSimpleName(), e);
+        }
+    }
+}
