fec7f1dc54d2d805550c3ae42b5ec30a4ed6f4a0
kirbloz <one.baisini@gmail.com>
2025-02-07 20:23:16 +0100

    inserito i file corretti nelle configurazioni

--- src/main/java/it/unibs/projectIngesoft/config/ErmesConfig.java
+++ src/main/java/it/unibs/projectIngesoft/config/ErmesConfig.java
@@ -2,12 +2,12 @@ package it.unibs.projectIngesoft.config;
 
 public class ErmesConfig {
 
-    private static final String FATTORI_DI_CONVERSIONE_JSON_FILEPATH = "fattoriTest.json";
-    private static final String UTENTI_JSON_FILEPATH = "usersTest.json";
+    private static final String FATTORI_DI_CONVERSIONE_JSON_FILEPATH = "fattori.json";
+    private static final String UTENTI_JSON_FILEPATH = "users.json";
     private static final String UTENTI_DEF_CREDS_JSON_FILEPATH = "defaultCredentials.json";
-    private static final String CATEGORIE_JSON_FILEPATH = "categorieTest.json";
-    private static final String COMPRENSORI_GEOGRAFICI_JSON_FILEPATH = "comprensoriGeograficiTest.json";
-    private static final String PROPOSTE_JSON_FILEPATH = "proposteTest.json";
+    private static final String CATEGORIE_JSON_FILEPATH = "categorie.json";
+    private static final String COMPRENSORI_GEOGRAFICI_JSON_FILEPATH = "comprensoriGeografici.json";
+    private static final String PROPOSTE_JSON_FILEPATH = "proposte.json";
 
     public String getFattoriPath() {
         return FATTORI_DI_CONVERSIONE_JSON_FILEPATH;
